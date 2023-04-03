b346e0f207caa86e4ca708c1a2634002b8b6781d
MarinaPrivalova <privalovama@yandex.ru>
2023-04-03 09:42:35 +0300

    feat: video play, pause, fix: z-index poster, btns

--- scripts/video-player.js
+++ scripts/video-player.js
@@ -1,4 +1,4 @@
-const player = document.querySelector('.video__clip');
+const video = document.querySelector('.video__clip');
 const videoPoster = document.querySelector('.video__poster');
 const playBtn = document.querySelector('.video__start-btn');
 const playControl = document.querySelector('.controls__play-btn');
@@ -7,6 +7,37 @@ const volumeBtn = document.querySelector('.controls__volume-btn');
 const progressVolume = document.querySelector('.controls__volume-bar');
 
 let isPlay = false;
-let prevVolume = player.volume;
+let prevVolume = video.volume;
 let value;
 
+function playVideo() {
+  playBtn.classList.add('video__start-btn_hide');
+  videoPoster.classList.add('video__poster_hide');
+  playControl.style.backgroundImage = 'url("../images/svg/pause.svg")';
+  isPlay = true;
+
+  video.play();
+}
+
+function pauseVideo() {
+  playBtn.classList.remove('video__start-btn_hide');
+  playControl.style.backgroundImage = 'url("../images/svg/play.svg")';
+  isPlay = false;
+
+  video.pause();
+}
+
+
+
+
+
+
+playBtn.addEventListener('click', playVideo);
+video.addEventListener('click', pauseVideo);
+playControl.addEventListener('click', () => {
+  if(isPlay) {
+    pauseVideo()
+  } else {
+    playVideo()
+  }
+})
